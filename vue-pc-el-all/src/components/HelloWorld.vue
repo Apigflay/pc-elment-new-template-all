1cfd614d571b48a949fc7b421c7ba79545ba31a9
姚晓辉 <283251605@qq.com>
2019-08-07 15:20:00 +0800

    a

---
+++ vue-pc-el-all/src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    {{this.Global.tagArry}}
+    <!-- <svg-icon icon-class="home"></svg-icon> -->
+  </div>
+</template>
+
+<script>
+import {square} from '../assets/js/Gfunction'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  created() {
+    this.goa()
+  },
+  methods: {
+    goa:function(){
+      console.log(square(2))
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ vue-pc-el-all/src/main.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+// import axios from 'axios';//引入axios
+import axios from './assets/js/axios';   //并依据创建文件
+Vue.prototype.$axios = axios; //添加axios到Vue的原型对象上
+
+import './assets/css/global.scss'
+
+import './assets/svgImg'
+
+// import NavBar from './components/component/Nav.vue';
+// Vue.component("NavBar",NavBar); // 全局注册组件
+
+import Global_ from './assets/js/Global'
+Vue.prototype.Global=Global_;
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
